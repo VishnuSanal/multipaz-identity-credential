46aec1d1da1f5e4e2b3008bc7af2f195d88e05b6
Vishnu Sanal T <vishnusanalt@gmail.com>
2026-03-04 14:47:18 +0530

    detekt test
    Signed-off-by: Vishnu Sanal T <vishnusanalt@gmail.com>

---
+++ multipaz-doctypes/src/commonMain/kotlin/org/multipaz/documenttype/knowntypes/VishnuID.kt
@@ -0,0 +1,34 @@
+package org.multipaz.documenttype.knowntypes
+
+import org.multipaz.cbor.toDataItem
+import org.multipaz.cbor.toDataItemFullDate
+import org.multipaz.documenttype.DocumentAttributeType
+import org.multipaz.documenttype.DocumentType
+import org.multipaz.documenttype.Icon
+import org.multipaz.util.fromBase64Url
+import kotlinx.datetime.LocalDate
+import org.multipaz.cbor.buildCborMap
+import org.multipaz.documenttype.knowntypes.DrivingLicense.MDL_NAMESPACE
+
+object VishnuID {
+    private const val PHOTO_ID_DOCTYPE = "org.iso.23220.vishnu.1"
+    private const val ISO_23220_2_NAMESPACE = "org.iso.23220.1"
+    private const val PHOTO_ID_NAMESPACE = "org.iso.23220.vishnu.1"
+    private const val DTC_NAMESPACE = "org.iso.23220.dtc.1"
+
+    fun getDocumentType(): DocumentType = with(DocumentType.Builder("Vishnu's ID")) {
+        addMdocDocumentType(PHOTO_ID_DOCTYPE)
+
+        // Data elements from ISO/IEC 23220-4 Table C.1 — PhotoID data elements defined by ISO/IEC TS 23220-2
+        addMdocAttribute(
+            DocumentAttributeType.String,
+            "family_name",
+            "Family Name",
+            "Last name, surname, or primary identifier, of the document holder",
+            true,
+            ISO_23220_2_NAMESPACE,
+            Icon.PERSON,
+            SampleData.FAMILY_NAME.toDataItem()
+        )
+    }.build()
+}
